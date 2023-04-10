a78fa0edb10c1716922acc96d55bfd8ebf7a0b89
techw <techwithfarmani@gmail.com>
2023-04-10 14:33:55 +0330

    Entity

---
+++ app/src/main/java/com/farmani/xcontact/entity/UserEntity.kt
@@ -0,0 +1,18 @@
+package com.farmani.xcontact.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+class UserEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "user_name")
+    var name: String,
+    @ColumnInfo(name = "user_email")
+    var email: String?,
+    @ColumnInfo(name = "user_number")
+    var number: String
+) {
+    constructor(name: String, email: String?, number: String) : this(0, name, email, number)
+}
